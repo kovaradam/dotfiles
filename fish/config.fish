632dd63e07624c32a4b546985fcd1aca246d53bc
Adam Kovar <kovaradam1@gmail.com>
2022-10-03 22:11:08 +0200

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias code="code-oss"
 alias vim="nvim"
 alias sus="systemctl suspend"
 alias off="poweroff"
 alias c="clear"
-
+alias ..="cd .."
