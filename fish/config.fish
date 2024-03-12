1efb1adc334f23885210b3bbc5441df3f309f29c
Adam Kovář <52539044+kovaradam@users.noreply.github.com>
2024-03-12 18:26:04 +0100

    🎨

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,13 @@ alias sus="systemctl suspend"
 alias off="poweroff"
 alias c="clear"
 alias ..="cd .."
+
+alias ys="yarn start"
+alias yys="yarn;yarn start"
+
+alias nr="npm run"
+
+alias gdef="git checkout (git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')"
+alias gm="git add .; git commit"
+alias gmp="git add .; git commit; git push"
+alias gma="git add .; git commit --amend; git push -f"
